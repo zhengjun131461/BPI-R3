924bd0ee87e3466b444148fae4482f128c20c1cb
zhengjun131461 <43834414+zhengjun131461@users.noreply.github.com>
2023-02-08 19:16:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'git clone https://github.com/4IceG/luci-app-sms-tool.git' package/luci-app-sms-tool
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
